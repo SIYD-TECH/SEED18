fb019b1542f9dc1ce0e700c296ca84e93305fdae
Solomon <olurebisolomon@gmail.com>
2025-11-03 12:09:18 -0800

    edited the footer and contact section redesigned the foter and contact section to give the sections a more welcomming and user friendle feel

--- index.js
+++ index.js
@@ -3,38 +3,6 @@ const mobileMenu = document.getElementById("mobile")
 const links = document.getElementsByClassName("link")
 const close = document.getElementById("closebtn")
 
-// hamBtn.addEventListener("click", () =>{
-//     mobileMenu.classList.toggle("hidden")
-// })
-
-// close.addEventListener("click", () => {
-//     mobileMenu.classList.add("hidden")
-// })
-
-// links.addEventListener.forEach("click", () => {
-//     mobileMenu.classList.add("hidden")
-//     // console.log(links)
-// })
-
-
-// hamBtn.addEventListener('click', () => {
-//       if (mobileMenu.classList.contains('hidden')) {
-//         // Show sidebar and start animation
-//         mobileMenu.classList.remove('hidden');
-//         requestAnimationFrame(() => {
-//           mobileMenu.classList.remove('-translate-x-full');
-//           mobileMenu.classList.add('translate-x-0');
-//         });
-//       } else {
-//         // Hide with animation, then fully hide
-//         mobileMenu.classList.remove('translate-x-0');
-//         mobileMenu.classList.add('-translate-x-full');
-//         // Wait for animation to finish before hiding completely
-//         setTimeout(() => {
-//           mobileMenu.classList.add('hidden');
-//         }, 300); // Match with duration-300
-//       }
-//     });
 
 hamBtn.addEventListener("click",() => {
     mobileMenu.classList.remove('hidden');
